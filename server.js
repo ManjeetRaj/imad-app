64688164b293c0d07041584deee4269c81ba60bd
ManjeetRaj <32019813+ManjeetRaj@users.noreply.github.com>
2018-03-04 01:21:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -106,6 +106,14 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+
+app.get('/:articleName',function(req,res) {
+    //articleName=article-one
+    //artilcles[articleName]=={} content object for article-one
+    var articleName = req.params.articleName;
+     res.send(createTemplate(articles[articleName]));
+});
+
 var pool = new Pool(config);
 app.get('/test-db', function(req ,res){
   pool.query('SELECT * from test',function(err,result) 
@@ -118,14 +126,6 @@ app.get('/test-db', function(req ,res){
     }
   });
 });
-
-app.get('/:articleName',function(req,res) {
-    //articleName=article-one
-    //artilcles[articleName]=={} content object for article-one
-    var articleName = req.params.articleName;
-     res.send(createTemplate(articles[articleName]));
-});
-
 
 
 app.get('/ui/style.css', function (req, res) {
