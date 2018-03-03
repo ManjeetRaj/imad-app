ed1f1a18432dfb6f0462d6b7f08f404ee9057dae
ManjeetRaj <32019813+ManjeetRaj@users.noreply.github.com>
2018-03-04 00:47:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -114,8 +114,8 @@ app.get('/:articleName',function(req,res) {
 });
 
 var pool = new Pool(config);
-app.get('/test-db', function(req ,res){
-  pool.query('select * from test',function(err,result) 
+app.get('/teat-db', function(req ,res){
+  pool.query('select * from teat',function(err,result) 
   {
     if(err){
         res.status(500).send(err.toString());
