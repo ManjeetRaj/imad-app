c393dfb6779e59453ae036fab48d244328387f14
ManjeetRaj <32019813+ManjeetRaj@users.noreply.github.com>
2018-03-04 00:18:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -113,20 +113,23 @@ app.get('/:articleName',function(req,res) {
      res.send(createTemplate(articles[articleName]));
 });
 
-/*var pool = new Pool(config);
+var pool = new Pool(config);
 
 app.get('/:test-db',function(req,res){
    //make a select request
    //return a response with the results
+   pool.connect();
    pool.query('SELECT * FROM test',function(err,result){
        if(err){
            res.status(500).send(err.toString());
        }else{
           // res.send(JSON.stringify(result.rows));
           res.send(result.toString());
+         
        }
+       pool.end();
     });
-});*/
+});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
